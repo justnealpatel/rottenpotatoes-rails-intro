483ccdcaeb9bc4e9cbc3eab2ea0d600ce6124736
Neal Patel <justnealpatel@gmail.com>
2018-02-16 16:20:27 -0600

    changed some previous code and part 3 complete with condition

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,9 +12,16 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.ratings
-    @sort = params[:sort]
-    params[:ratings].nil? ? rating_param = @all_ratings : rating_param = params[:ratings].keys
-    @movies = Movie.where(rating: rating_param).order(@sort)
+    @sort = params[:sort] || session[:sort]
+    session[:ratings] = session[:ratings] || {'G' => '', 'PG' => '', 'PG-13' => '', 'R' => ''}
+    @rating_param = params[:ratings] || session[:ratings]
+    session[:sort] = @sort
+    session[:ratings] = @rating_param
+    @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    if (params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
+      flash.keep
+      redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
+    end
   end
 
   def new
