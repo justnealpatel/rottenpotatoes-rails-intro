d09dd27ada48bce88f4915a45fc5db8552d07679
Neal Patel <justnealpatel@gmail.com>
2018-02-16 14:41:07 -0600

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,10 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.ratings
     @sort = params[:sort]
-    @movies = Movie.all.order(@sort)
+    params[:ratings].nil? ? rating_param = @all_ratings : rating_param = params[:ratings].keys
+    @movies = Movie.where(rating: rating_param).order(@sort)
   end
 
   def new
